19ce1a58bd83775dfad00b89b0b5121d281e15ea
Hong <honghub@gmail.com>
2017-10-22 10:38:18 +0700

    On click on first page

--- app/src/main/java/app/mellow/hong/myeasyservice/fragment/MainFragment.java
+++ app/src/main/java/app/mellow/hong/myeasyservice/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import app.mellow.hong.myeasyservice.R;
 
@@ -20,5 +21,30 @@ public class MainFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         return view;
+    }
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+        //button controller
+        buttonController();
+
+
+    }// Main Method
+
+    private void buttonController() {
+        //initial view
+        Button button = getView().findViewById(R.id.btnGoToSecond);
+        //Get event from click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //move to second fragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentFragmentMain, new SecondFragment()).addToBackStack(null).commit();
+            } //on click action
+        });
     }
 }//Main Class
