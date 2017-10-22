02081666c9736fc56b0d9ad17283a003597e0539
Hong <honghub@gmail.com>
2017-10-22 11:06:36 +0700

    On Back Controller work

--- app/src/main/java/app/mellow/hong/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/app/mellow/hong/myeasyservice/fragment/SecondFragment.java
@@ -1,11 +1,14 @@
 package app.mellow.hong.myeasyservice.fragment;
 
+import android.accessibilityservice.AccessibilityButtonController;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import app.mellow.hong.myeasyservice.R;
 
@@ -22,4 +25,27 @@ public class SecondFragment extends Fragment {
         return  view;
 
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        BackController();
+        //button.setOnClickListener(ne);
+    }
+
+    private void BackController() {
+        //Back Controller
+        Button button = getView().findViewById(R.id.btnBack);
+        //back Action
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 //move back to first page
+                getActivity().getSupportFragmentManager().popBackStack();
+
+
+            }
+        });
+    }
+
 }
