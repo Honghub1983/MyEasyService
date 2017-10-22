366e774d16978b8df5efad0fd45eab05b08268ca
Hong <honghub@gmail.com>
2017-10-22 16:02:01 +0700

    Finish workshop

--- app/src/main/java/app/mellow/hong/myeasyservice/MainActivity.java
+++ app/src/main/java/app/mellow/hong/myeasyservice/MainActivity.java
@@ -1,16 +1,25 @@
 package app.mellow.hong.myeasyservice;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import app.mellow.hong.myeasyservice.fragment.MainFragment;
+import app.mellow.hong.myeasyservice.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +33,54 @@ public class MainActivity extends AppCompatActivity {
         //Text Controller
         textController();
 
+        //CreateToolbar
+        createToolbar();
     }// Main Method
+
+    // to add action on hamburger key
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    //tomade hamburgerkey
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    //tomade hamburgerkey
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void createToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.ToolbarMain);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
 
     private void textController() {
         TextView MainTextView = (TextView) findViewById(R.id.txtMain);
-        TextView    SecondTextView = (TextView) findViewById(R.id.txtSecond);
-        TextView    ExitTextView   = (TextView) findViewById(R.id.txtExit);
+        TextView SecondTextView = (TextView) findViewById(R.id.txtSecond);
+        TextView ExitTextView = (TextView) findViewById(R.id.txtExit);
 
         //For MainFragment
         MainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 drawerLayout.closeDrawers();
+                getSupportFragmentManager().beginTransaction().replace(R.id.contentFragmentMain, new MainFragment()).commit();
+                drawerLayout.closeDrawers();
             }
         });
 
@@ -43,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         SecondTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.contentFragmentMain, new SecondFragment()).commit();
                 drawerLayout.closeDrawers();
             }
         });
@@ -50,18 +96,19 @@ public class MainActivity extends AppCompatActivity {
         ExitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
                 drawerLayout.closeDrawers();
             }
         });
     }
 
     private void setupDrawerMenu() {
-        drawerLayout= (DrawerLayout) findViewById(R.id.MyDrawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.MyDrawer);
     }
 
     private void addFragment(Bundle savedInstanceState) {
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.contentFragmentMain,new MainFragment()).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.contentFragmentMain, new MainFragment()).commit();
         }
     }
 
--- app/src/main/java/app/mellow/hong/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/app/mellow/hong/myeasyservice/fragment/SecondFragment.java
@@ -1,7 +1,11 @@
 package app.mellow.hong.myeasyservice.fragment;
 
 import android.accessibilityservice.AccessibilityButtonController;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import app.mellow.hong.myeasyservice.R;
 
@@ -17,6 +23,9 @@ import app.mellow.hong.myeasyservice.R;
  */
 
 public class SecondFragment extends Fragment {
+
+    private ImageView imageView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -31,7 +40,43 @@ public class SecondFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         BackController();
         //button.setOnClickListener(ne);
+
+        //Image Controller
+        imageController();
+    }//Main
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK) {
+            try {
+                Uri uri =data.getData();
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+        } else {
+            Toast.makeText(getActivity(),"Not Choose Image",Toast.LENGTH_LONG).show();
+        }
     }
+
+    private void imageController() {
+        imageView =getView().findViewById(R.id.imgPhoto);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent.createChooser(intent,"Please Choose App"),8);
+            }
+        });
+    }
+
 
     private void BackController() {
         //Back Controller
